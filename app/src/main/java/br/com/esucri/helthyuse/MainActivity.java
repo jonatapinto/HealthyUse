d917c0c523dc38c218b0d8ad5f9ef8179d0e0962
SaymonSilva <42623808+SaymonSilva@users.noreply.github.com>
2018-08-27 23:32:50 -0300

    Saymon

--- app/src/main/java/br/com/esucri/helthyuse/MainActivity.java
+++ app/src/main/java/br/com/esucri/helthyuse/MainActivity.java
@@ -2,12 +2,24 @@ package br.com.esucri.helthyuse;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button botaoOficios;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        botaoOficios = (Button) findViewById(R.id.botaoOficios);
+        botaoOficios.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                /*chamar outra tela*/
+            }
+        });
     }
 }

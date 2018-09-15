dda990b5451a57b58357aa974a80e8e51fd4f7b6
Jonata Pinto <jonatasilvapinto@gmail.com>
2018-09-15 20:38:18 -0300

    Ajuste inserção Rotina
    Ajuste da rotina de inserção de rotinas e criado update de rotinas.

--- app/src/main/java/br/com/esucri/healthyUse/RotinaActivity.java
+++ app/src/main/java/br/com/esucri/healthyUse/RotinaActivity.java
@@ -1,12 +1,16 @@
 package br.com.esucri.healthyUse;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.text.ParseException;
 
@@ -20,9 +24,10 @@ public class RotinaActivity extends AppCompatActivity {
     RadioButton radioDomingo, radioSegunda, radioTerca, radioQuarta, radioQuinta, radioSexta,
             radioSabado, radioWhatsApp, radioInstagram, radioFacebook;
     Button botaoGravarRotina;
-    Rotina editarRotina, rotina = new Rotina();
+    TextView textViewAplicativos;
+    Rotina editarRotina = new Rotina();
     Parsers parsers = new Parsers();
-    RotinaController rotinaController;
+    Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +37,7 @@ public class RotinaActivity extends AppCompatActivity {
         Intent intent = getIntent();
         editarRotina = (Rotina) intent.getSerializableExtra("rotina_escolhida");
 
+        //Buscar valores da activity por ID
         editNome = (EditText) findViewById(R.id.editNome);
         editTipo = (EditText) findViewById(R.id.editTipo);
         editHoraInicio = (EditText) findViewById(R.id.editHoraInicio);
@@ -48,43 +54,107 @@ public class RotinaActivity extends AppCompatActivity {
         radioInstagram = (RadioButton) findViewById(R.id.radioInstagram);
         radioFacebook = (RadioButton) findViewById(R.id.radioFacebook);
         botaoGravarRotina = (Button) findViewById(R.id.botaoGravarRotina);
+        textViewAplicativos = (TextView) findViewById(R.id.textViewAplicativos);
 
-        if(editarRotina !=null){
-            botaoGravarRotina.setText("Modificar");
-        } else{
-            botaoGravarRotina.setText("Gravar");
-        }
+        //if(editarRotina !=null){
+        //    botaoGravarRotina.setText("Modificar");
+        //} else{
+        //    botaoGravarRotina.setText("Gravar");
+        //}
 
         botaoGravarRotina.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                try {
-                    rotina.setNome(editNome.getText().toString());
-                    rotina.setTipo(editTipo.getText().toString());
-                    rotina.setHoraInicio(parsers.parserStringToTime(editHoraInicio.getText().toString()));
-                    rotina.setHoraFinal(parsers.parserStringToTime(editHoraFinal.getText().toString()));
-                    rotina.setDataFinal(parsers.parserStringToDate(editDataFinal.getText().toString()));
-                    rotina.setDom(radioDomingo.isChecked()?1:0);
-                    rotina.setSeg(radioSegunda.isChecked()?1:0);
-                    rotina.setTer(radioTerca.isChecked()?1:0);
-                    rotina.setQua(radioQuarta.isChecked()?1:0);
-                    rotina.setQui(radioQuinta.isChecked()?1:0);
-                    rotina.setSex(radioSexta.isChecked()?1:0);
-                    rotina.setSab(radioSabado.isChecked()?1:0);
-                    rotina.setWhatsapp(radioWhatsApp.isChecked()?1:0);
-                    rotina.setFacebook(radioFacebook.isChecked()?1:0);
-                    rotina.setInstagram(radioInstagram.isChecked()?1:0);
-
-                    if(botaoGravarRotina.getText().equals("Gravar")){
-                        rotinaController.create(rotina);
-                    }
-
-                } catch (ParseException e) {
-                    e.printStackTrace();
-                }
+                salvar(v);
             }
         });
     }
 
+    public void salvar(View view) {
 
+        if (!validaCampos()) {
+            return;
+        }
+
+        try {
+            Rotina rotina = new Rotina();
+            rotina.setNome(editNome.getText().toString());
+            rotina.setTipo(editTipo.getText().toString());
+            rotina.setHoraInicio(parsers.parserStringToTime(editHoraInicio.getText().toString()));
+            rotina.setHoraFinal(parsers.parserStringToTime(editHoraFinal.getText().toString()));
+            rotina.setDataFinal(parsers.parserStringToDate(editDataFinal.getText().toString()));
+            rotina.setDom(radioDomingo.isChecked()?1:0);
+            rotina.setSeg(radioSegunda.isChecked()?1:0);
+            rotina.setTer(radioTerca.isChecked()?1:0);
+            rotina.setQua(radioQuarta.isChecked()?1:0);
+            rotina.setQui(radioQuinta.isChecked()?1:0);
+            rotina.setSex(radioSexta.isChecked()?1:0);
+            rotina.setSab(radioSabado.isChecked()?1:0);
+            rotina.setWhatsapp(radioWhatsApp.isChecked()?1:0);
+            rotina.setFacebook(radioFacebook.isChecked()?1:0);
+            rotina.setInstagram(radioInstagram.isChecked()?1:0);
+
+            RotinaController crud = new RotinaController(getBaseContext());
+
+            long retorno;
+            //if (TextUtils.isEmpty(idProduto)) {
+                retorno = crud.create(rotina);
+            //} else {
+            //    produto.setId(Integer.parseInt(idProduto));
+            //    retorno = crud.update(produto);
+            //}
+
+            if (retorno == -1) {
+                Toast.makeText(RotinaActivity.this, "Erro ao salvar o registro!", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(RotinaActivity.this, "Registro salvo com sucesso!", Toast.LENGTH_LONG).show();
+                //limpar();
+            }
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private boolean validaCampos() {
+        Boolean result = true;
+        String textoDescricao;
+        Boolean marcacaoRadio;
+
+        textoDescricao = editNome.getText().toString().trim();
+        if (TextUtils.isEmpty(textoDescricao)) {
+            editNome.requestFocus();
+            editNome.setError("Campo obrigatório!");
+            return false;
+        }
+
+        textoDescricao = editHoraInicio.getText().toString().trim();
+        if (TextUtils.isEmpty(textoDescricao)) {
+            editHoraInicio.requestFocus();
+            editHoraInicio.setError("Campo obrigatório!");
+            return false;
+        }
+
+        textoDescricao = editHoraFinal.getText().toString().trim();
+        if (TextUtils.isEmpty(textoDescricao)) {
+            editHoraFinal.requestFocus();
+            editHoraFinal.setError("Campo obrigatório!");
+            return false;
+        }
+
+        //Verifica se algum dos aplicativos foi selecionados
+        marcacaoRadio = radioFacebook.isChecked();
+        if (!marcacaoRadio) {
+            marcacaoRadio = radioWhatsApp.isChecked();
+            if (!marcacaoRadio){
+                marcacaoRadio = radioInstagram.isChecked();
+                if (!marcacaoRadio){
+                    Toast.makeText(RotinaActivity.this, "Favor selecionar algum aplicativo!", Toast.LENGTH_LONG).show();
+                    textViewAplicativos.setError("Opção obrigatória!");
+                    return false;
+                }
+            }
+        }
+
+        return result;
+    }
 }
--- app/src/main/java/br/com/esucri/healthyUse/controller/RotinaController.java
+++ app/src/main/java/br/com/esucri/healthyUse/controller/RotinaController.java
@@ -1,6 +1,7 @@
 package br.com.esucri.healthyUse.controller;
 
 import android.content.ContentValues;
+import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 
@@ -17,6 +18,10 @@ import br.com.esucri.healthyUse.utils.Parsers;
 public class RotinaController {
     private SQLiteDatabase instanciaDB;
     private BancoDeDados db;
+
+    public RotinaController(Context context) {
+        db = new BancoDeDados(context);
+    }
 
     public long create(final Rotina rotina){
         ContentValues dados = new ContentValues();
@@ -76,9 +81,34 @@ public class RotinaController {
         return rotinas;
     }
 
-    //public long update(final Rotina rotina){
-    //    return 1;
-    //}
+    public long update(final Rotina rotina){
+        ContentValues dados = new ContentValues();
+        long resutado;
+
+        instanciaDB = db.getWritableDatabase();
+        dados.put("NOME", rotina.getNome());
+        dados.put("TIPO", rotina.getTipo());
+        dados.put("HORA_INICIO", rotina.getHoraInicio().toString());
+        dados.put("HORA_FINAL", rotina.getHoraFinal().toString());
+        dados.put("DOM", rotina.getDom());
+        dados.put("SEG", rotina.getSeg());
+        dados.put("TER", rotina.getTer());
+        dados.put("QUA", rotina.getQua());
+        dados.put("QUI", rotina.getQui());
+        dados.put("SEX", rotina.getSex());
+        dados.put("SAB", rotina.getSab());
+        dados.put("INSTAGRAM", rotina.getInstagram());
+        dados.put("FACEBOOK", rotina.getFacebook());
+        dados.put("WHATSAPP", rotina.getWhatsapp());
+        dados.put("DATA_FINAL", rotina.getDataFinal().toString());
+
+        String where = "ID = " + rotina.getId();
+
+        resutado = instanciaDB.update("ROTINA", dados, where, null);
+        instanciaDB.close();
+
+        return resutado;
+    }
 
     //public long delete(final Rotina rotina) {
     //    return 1;
